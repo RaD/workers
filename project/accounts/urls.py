6006dab1cb87e8bfe00ca363b1e389853a898af1
Ruslan Popov <ruslan.popov@gmail.com>
2021-11-06 22:45:33 +0300

    Вход пользователя

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from project.accounts import views
@@ -5,4 +6,5 @@ from project.accounts import views
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('register/done/', views.RegisterDoneView.as_view(), name='register_done'),
+    path('login/', LoginView.as_view(), name='login'),
     ]
