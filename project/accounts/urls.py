ddbaf02bd71ef886c9341933f39e101099b96161
Ruslan Popov <ruslan.popov@gmail.com>
2021-11-07 13:08:49 +0300

    Выход пользователя

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from project.accounts import views
@@ -7,4 +7,5 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('register/done/', views.RegisterDoneView.as_view(), name='register_done'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     ]
--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'project.accounts',
+    'project.slaves',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,9 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'django_bootstrap5',
-
-    'project.accounts',
-    'project.slaves',
 ]
 
 MIDDLEWARE = [
